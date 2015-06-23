c1efa26a241277f23b435f03f42aa64563003295
Gary Duncan <g@shedcollective.org>
2015-06-23 16:00:07 +0100

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+var _APP;
+_APP = function()
+{
+    /**
+     * Avoid scope issues in callbacks and anonymous functions by referring to `this` as `base`
+     * @type {Object}
+     */
+    var base = this;
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Construct the class
+     * @return {Void}
+     */
+    base.__construct = function()
+    {
+
+        //  PUT ANY JS YOU HAVE HERE
+
+    };
+
+    // --------------------------------------------------------------------------
+
+    return base.__construct();
+}();
